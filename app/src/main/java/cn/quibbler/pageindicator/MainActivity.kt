5fef280dbbf3589799b28386299e667224608fa7
quibbler01 <fluent0418@gmail.com>
2023-06-20 23:00:06 +0800

    demo how to use this PageIndicator

--- app/src/main/java/cn/quibbler/pageindicator/MainActivity.kt
+++ app/src/main/java/cn/quibbler/pageindicator/MainActivity.kt
@@ -2,11 +2,17 @@ package cn.quibbler.pageindicator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.viewpager.widget.PagerAdapter
+import androidx.viewpager.widget.ViewPager
 import cn.quibbler.pageindicator.databinding.ActivityMainBinding
+import cn.quibbler.pageindicator.databinding.PageItemBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding:ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,8 +22,37 @@ class MainActivity : AppCompatActivity() {
         initViews()
     }
 
-    private fun initViews(){
+    private fun initViews() {
+        binding.viewPager.adapter = DemoPageAdapter()
+        binding.viewPager.addOnPageChangeListener(object : ViewPager.OnPageChangeListener {
+            override fun onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int) {
+            }
 
+            override fun onPageSelected(position: Int) {
+            }
+
+            override fun onPageScrollStateChanged(state: Int) {
+            }
+        })
+
+        binding.pageIndicator.attachTo(binding.viewPager)
+    }
+
+    class DemoPageAdapter : PagerAdapter() {
+
+        override fun getCount(): Int = 5
+
+        override fun instantiateItem(container: ViewGroup, position: Int): Any {
+            val binding = PageItemBinding.inflate(LayoutInflater.from(container.context), container, false)
+            container.addView(binding.root)
+            return binding.root
+        }
+
+        override fun destroyItem(container: ViewGroup, position: Int, `object`: Any) {
+            container.removeView(`object` as View)
+        }
+
+        override fun isViewFromObject(view: View, `object`: Any): Boolean = view == `object`
     }
 
 }
