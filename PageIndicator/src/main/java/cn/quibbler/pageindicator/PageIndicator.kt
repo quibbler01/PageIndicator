d42af31a008a7b15c994aa27b08763e99f4d466e
quibbler01 <fluent0418@gmail.com>
2023-06-25 21:36:04 +0800

    Move BaseSavedState to PageIndicator class

--- PageIndicator/src/main/java/cn/quibbler/pageindicator/PageIndicator.kt
+++ PageIndicator/src/main/java/cn/quibbler/pageindicator/PageIndicator.kt
@@ -5,6 +5,7 @@ import android.content.Context
 import android.content.res.Resources
 import android.graphics.Canvas
 import android.graphics.Paint
+import android.os.Parcel
 import android.os.Parcelable
 import android.util.AttributeSet
 import android.view.View
@@ -236,6 +237,43 @@ class PageIndicator constructor(context: Context, attrs: AttributeSet?, defStyle
         return Pair(start, end)
     }
 
+    class SavedState : BaseSavedState {
 
+        var count: Int = 0
+
+        var selectedIndex = 0
+
+        constructor(superState: Parcelable) : super(superState)
+
+        constructor(source: Parcel?) : this(source, null) {
+            count = source?.readInt() ?: 0
+            selectedIndex = source?.readInt() ?: 0
+        }
+
+        constructor(source: Parcel?, loader: ClassLoader?) : super(source, loader) {
+            this.count = source?.readInt() ?: 0
+            this.selectedIndex = source?.readInt() ?: 0
+        }
+
+        override fun writeToParcel(out: Parcel, flags: Int) {
+            super.writeToParcel(out, flags)
+            out.writeInt(this.count)
+            out.writeInt(this.selectedIndex)
+        }
+
+        companion object {
+            @JvmField
+            val CREATOR: Parcelable.Creator<SavedState> = object : Parcelable.Creator<SavedState> {
+                override fun createFromParcel(parcel: Parcel): SavedState {
+                    return SavedState(parcel)
+                }
+
+                override fun newArray(size: Int): Array<SavedState?> {
+                    return arrayOfNulls(size)
+                }
+            }
+        }
+
+    }
 
 }
